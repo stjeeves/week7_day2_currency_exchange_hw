91f83abf50ffe24526b92167de7ef7afb1b296fc
Stephen <st-jeeves@hotmail.co.uk>
2020-03-31 18:30:20 +0100

    half of mvp done

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el: "#app",
+        data: {
+            currencies: [],
+            selectedCurrency: null
+        },
+        mounted(){
+            this.fetchCurrencies();
+        },
+        computed: {
+            allCurrencies: function(){
+                return this.currencies
+            },
+
+            // convertCurrency: function(){
+            //     return this.currencie
+            // }
+            // selectedCurrencyFunction: function () {
+            //     return this.currencies.filter()
+            // }
+        },
+        methods: {
+            fetchCurrencies: function(){
+                fetch('https://api.exchangeratesapi.io/latest')
+                    .then(response => response.json())
+                    .then(data => this.currencies = data)
+                    .catch(err => console.error(err))
+            },
+            
+
+        }
+    })
+})
